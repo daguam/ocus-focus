350f657eee805938666a8208b122932fb019bfd2
daguam <daguam93@gmail.com>
2019-06-05 21:35:01 -0500

    Initial project version

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+myButton.onclick = function() {
+    setUsername();
+}
+
+if(!localStorage.getItem('name')) {
+    setUsername();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = "Loading name at inicialization " + storedName;
+}
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/imgOcusFocus.png') {
+        myImage.setAttribute('src','images/imgOcusFocus2.png');
+    } else {
+        myImage.setAttribute('src','images/imgOcusFocus.png');
+    }
+}
+
+function setUsername() {
+    var myName = prompt('Please enter yoru name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Loading name at user change or first visit, ' + myName;
+}
