32070e027da8efd800e1a2217910f82d349a3aca
daguam <daguam93@gmail.com>
2019-06-20 22:51:55 -0500

    Add comments to main

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+//Creates variables selecting the first img, button and h1
 var myImage = document.querySelector('img');
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
 
+//Checks for stored username or sets a new one if there is none
 if(!localStorage.getItem('name')) {
     setUsername();
 } else {
@@ -9,12 +11,11 @@ if(!localStorage.getItem('name')) {
     var storedName = localStorage.getItem('name');
     myHeading.textContent = "Welcome Back to Ocus Focus " + storedName + "!";
 }
-
+//Sets new username
 myButton.onclick = function() {
     setUsername();
 }
-
-
+//Changes de color of the image on click event
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/imgOcusFocus.png') {
@@ -23,7 +24,7 @@ myImage.onclick = function() {
         myImage.setAttribute('src','images/imgOcusFocus.png');
     }
 }
-
+//Set username using localstorage
 function setUsername() {
     var myName = prompt('Please enter yoru name.');
     if (myName != null) {
